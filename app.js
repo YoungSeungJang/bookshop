adb682f2cfd46f2810642478dc17e5441d541438
YoungSeungJang <1345620@naver.com>
2025-05-13 15:15:31 +0900

    :tada: bookshop Init

---
+++ app.js
@@ -0,0 +1,24 @@
+const path = require('path');
+// dotenv 모듈
+const dotenv = require('dotenv');
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+// express 모듈
+const express = require('express');
+const app = express();
+
+const userRouter = require('./routes/users');
+const bookRouter = require('./routes/books');
+const likeRouter = require('./routes/likes');
+const cartRouter = require('./routes/carts');
+const orderRouter = require('./routes/orders');
+
+app.use('/users', userRouter);
+app.use('/books', bookRouter);
+app.use('/likes', likeRouter);
+app.use('/carts', cartRouter);
+app.use('/orders', orderRouter);
+
+app.listen(process.env.PORT, () => {
+  console.log('server running', process.env.PORT);
+});
